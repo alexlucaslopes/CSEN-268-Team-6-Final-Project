0a5ef9d315957cc6fd0658b2d111c4e5dce1d597
Gohn-666 <119533875+Gohn-666@users.noreply.github.com>
2025-11-13 19:41:36 -0800

    Update main.dart

--- easy_list/lib/main.dart
+++ easy_list/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const EasyListerApp());
 }
