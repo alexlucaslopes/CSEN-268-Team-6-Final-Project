5867db13dea43f640648a008bbe4ba7781bca63a
Gohn-666 <119533875+Gohn-666@users.noreply.github.com>
2025-11-13 19:46:26 -0800

    Update home.dart

--- easy_list/lib/features/home/home.dart
+++ easy_list/lib/features/home/home.dart
@@ -1,4 +1,4 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
