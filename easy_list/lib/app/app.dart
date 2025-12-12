781e1abf3fbe6e12f82159baade1d6f4cead5cb0
AndrewLe4 <160354249+AndrewLe4@users.noreply.github.com>
2025-12-11 22:02:44 -0800

    Update app.dart

--- easy_list/lib/app/app.dart
+++ easy_list/lib/app/app.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
-//import 'package:go_router/go_router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'router.dart';
 import 'theme.dart';
 import 'package:easy_list/features/auth/bloc/bloc.dart';
 
-
-
-class EasyListerApp extends StatelessWidget {
+class EasyListerApp extends StatefulWidget {
   const EasyListerApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final authBloc = context.watch<AuthenticationBloc>();
-    //final GoRouter router = buildRouter(context);
+  State<EasyListerApp> createState() => _EasyListerAppState();
+}
 
+class _EasyListerAppState extends State<EasyListerApp> {
+  late final dynamic _router;
+
+  @override
+  void initState() {
+    super.initState();
+    final authBloc = context.read<AuthenticationBloc>();
+    _router = buildRouter(authBloc);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'EasyLister',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      routerConfig: buildRouter(authBloc),
+      routerConfig: _router,
     );
   }
 }
