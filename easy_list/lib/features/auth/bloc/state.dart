6b4d5b48ea2dcc6f41e348394aa85c33b7bbc3af
AndrewLe4 <160354249+AndrewLe4@users.noreply.github.com>
2025-12-11 22:03:17 -0800

    Update state.dart

--- easy_list/lib/features/auth/bloc/state.dart
+++ easy_list/lib/features/auth/bloc/state.dart
@@ -1,13 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-abstract class AuthenticationState {}
+abstract class AuthenticationState {
+  const AuthenticationState();
+}
 
-class AuthenticationInitial extends AuthenticationState {}
+class AuthenticationInitial extends AuthenticationState {
+  @override
+  bool operator ==(Object other) => other is AuthenticationInitial;
+  @override
+  int get hashCode => 0;
+}
 
-class AuthenticationUnauthenticated extends AuthenticationState {}
+class AuthenticationUnauthenticated extends AuthenticationState {
+  @override
+  bool operator ==(Object other) => other is AuthenticationUnauthenticated;
+  @override
+  int get hashCode => 1;
+}
 
 class AuthenticationAuthenticated extends AuthenticationState {
   final User user;
-  AuthenticationAuthenticated({required this.user});
-}
+  const AuthenticationAuthenticated({required this.user});
 
+  @override
+  bool operator ==(Object other) => 
+    other is AuthenticationAuthenticated && other.user.uid == user.uid;
+  
+  @override
+  int get hashCode => user.uid.hashCode;
+}
